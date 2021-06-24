7f960a4d0859b768328770bd6eeeb4a8314ef371
LuKendziur <lukendziur@hotmail.com>
2021-06-24 18:08:42 -0300

    deje harcodeado el delete y el destroy

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -14,7 +14,18 @@ router.get('/stock', adminController.stock);
 router.get('/create', adminController.create);
 router.post('/create', upload.single('image'), adminController.add);
 
-router.get('/stock/:id', adminController.delete); 
-router.delete('/stock/:id', adminController.destroy)
+// router.get('/stock/edit/:id', adminController.edit);
+// router.put('/stock/update/:id', adminController.update);
+
+//Para estas rutas vamos a hacer lo siguiente: la ruta pasará a ser /stock/delete/:id
+// vamos a crear una mini vista, en donde cuando apretemos el botón de eliminar producto, te pregunte si querés eliminarlo.
+// router.get('/stock/:id', adminController.delete);  //rutas de antes
+// router.delete('/stock/:id', adminController.destroy)
+
+router.get('/stock/delete/:id', adminController.delete); 
+router.post('/stock/delete/:id', adminController.destroy); 
+
+
+
 
 module.exports = router;
